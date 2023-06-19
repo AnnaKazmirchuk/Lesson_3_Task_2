cc506fd7899dcb007ee714ff5111ccbc396bb67a
Anna Kazmirchuk <anyuta32@mail.ru>
2023-06-19 21:08:57 +0300

    Initial commit

---
+++ src/BmiService.java
@@ -0,0 +1,7 @@
+public class BmiService {
+    public int calculate(int weightKg, double heightMeters) {
+        double index = weightKg / (heightMeters * heightMeters);
+        return (int) index;
+    }
+
+}
